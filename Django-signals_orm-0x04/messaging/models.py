4e43eef101a9cd3864f30932e28dc7ddf4a39623
unknown <abebeduguma27@gmail.com>
2025-08-03 21:16:20 +0300

    Create a Signal for Logging Message Edits

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -3,28 +3,25 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
-from django.db import models
-from django.conf import settings
-
-User = settings.AUTH_USER_MODEL
-
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Fields related to edits
     edited = models.BooleanField(default=False)
     edited_at = models.DateTimeField(null=True, blank=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
 
+    def __str__(self):
+        return f'Message from {self.sender} to {self.receiver}'
 
 class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     previous_content = models.TextField()
     updated_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 
-
-class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='notifications')
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'Edit on message {self.message.id} at {self.updated_at}'
